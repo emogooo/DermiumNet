06f9d50503263206deca34d009fec8de8171338e
Emirhan Aydın <58745898+emogooo@users.noreply.github.com>
2024-10-09 08:26:25 +0300

    started changing parameters to float

--- main.py
+++ main.py
@@ -28,16 +28,23 @@ denseLayers = [1, 2, 3, 4]
 numberOfNeurons = [16, 32, 64, 96, 112, 128, 144, 160, 176, 192, 256, 512]  # Tekrarlanan
 
 
-print(convolutionLayers[-1])
+# print(convolutionLayers[-1])
 
-print(random() * numberOfFilters[-1])  # bu doğru değil çünkü aralıklar çok büyük. 16-32 küçük, 256-512 çok büyük (eşit olması lazım)
-print(numberOfFilters[round(random() * (len(numberOfFilters) - 1))])  # bu doğru, değişkende int değeri tutmayacaksın unutma! kesirli değeri tutacaksın ki değer değişebilsin. yoksa değişim oranı en az 1 olmalı ki parametre değişimi olsun.
+# print(random() * numberOfFilters[-1])  # bu doğru değil çünkü aralıklar çok büyük. 16-32 küçük, 256-512 çok büyük (eşit olması lazım)
+# print(numberOfFilters[round(random() * (len(numberOfFilters) - 1))])  # bu doğru, değişkende int değeri tutmayacaksın unutma! kesirli değeri tutacaksın ki değer değişebilsin. yoksa değişim oranı en az 1 olmalı ki parametre değişimi olsun.
 
 maxConvolutionLayer = convolutionLayers[-1]
 maxDenseLayer = denseLayers[-1]
 
 
 class Agent:
+    """
+    Tüm parametre değerleri float olarak tutulmalı. Tutulan değerler ilgili dizinin index değerleridir. Gerçek değerler değil. Bu şekilde tutulma amacı ise, evrimsel algoritmanın her optimizasyonunda değer ufak ufak değişeceği için önceki değişiklikleri
+    kaybetmememiz lazım. Ya da değişikliklerin 1'den büyük olması lazım ki her optimizasyonda farklı bir hiperparametre listesi oluşsun (Tabii ki bu adımları çok büyük tutacağı için local minimumu atlamak anlamına gelir,
+    dolayısıyla bu çözüm yöntemi mantıklı değildir). Her optimizasyonda değerlerimiz azar azar değişecektir. Eğer biz bu değişiklikleri her optimizasyonda 16,32,64,96,128 gibi liste değerlerine ayarlarsak evrimsel algoritma sürekli aynı
+    hiperparametre listesini üretir. Çünkü değişim oranı, bu sınırlar içerisinde kalabilir. Zaman içerisinde değişim istiyorsak önceki değişikliklerin kaydedilmesi şarttır. İşte bu yüzden getHyperparameterList isimli bir metodumuz mevcuttur.
+    """
+
     def __init__(self, convolution: int, dense: int, filters: list, neurons: list) -> None:
         self.convolution = convolution
         self.dense = dense
@@ -45,8 +52,12 @@ class Agent:
         self.neurons = neurons
 
     def __str__(self):
-        return f"Convolution Layer: {self.convolution}, Dense Layer: {self.dense}, Filters: {self.filters}, Neurons: {self.neurons}"
-
+        roundedFilters = [round(x, 2) for x in self.filters]
+        roundedNeurons = [round(x, 2) for x in self.neurons]
+        return f"Convolution Layer: {round(self.convolution, 2)}, Dense Layer: {round(self.dense, 2)}, Filters: {roundedFilters}, Neurons: {roundedNeurons}"
+    
+    def setHyperparameterList():
+        pass
 
 class Thesis:
     def __init__(self) -> None:
@@ -55,55 +66,53 @@ class Thesis:
         self.DENSE_LAYERS = [1, 2, 3, 4]
         self.NUMBER_OF_NEURONS = [16, 32, 64, 96, 112, 128, 144, 160, 176, 192, 256, 512]  # Tekrarlanan
 
-    def createFirstAgents(self, number: int):
+    def createFirstAgents(self, number: int) -> list:
         agentList = []
         for _ in range(number):
-            conv = choice(self.CONVOLUTION_LAYERS)
-            dense = choice(self.DENSE_LAYERS)
+            conv = random() * (len(self.CONVOLUTION_LAYERS) - 1)
+            dense = random() * (len(self.DENSE_LAYERS) - 1)
 
             filters = []
             for _ in range(max(self.CONVOLUTION_LAYERS)):
-                filters.append(choice(self.NUMBER_OF_FILTERS))
+                filters.append(random() * (len(self.NUMBER_OF_FILTERS) - 1))
 
             neurons = []
             for _ in range(max(self.DENSE_LAYERS)):
-                neurons.append(choice(self.NUMBER_OF_NEURONS))
+                neurons.append(random() * (len(self.NUMBER_OF_NEURONS) - 1))
 
             agent = Agent(conv, dense, filters, neurons)
             agentList.append(agent)
 
         return agentList
 
-    def giveRandomSampleFrom(self, array: list):
-        idx = randint(0, len(array) - 1)
-        return array[idx]
+    def getHyperparameterList(self, reference: Agent) -> list:
+        """
+        Index değerlerini tam sayıya yuvarlayıp dizinin ilişkili elemanını alır, tüm elemanları aldıktan sonra dizi halinde döner.        
+        """
+        conv = self.CONVOLUTION_LAYERS[round(reference.convolution)]
+        dense = self.DENSE_LAYERS[round(reference.dense)]
+        filters = []
+        for i in range(len(reference.filters)):
+            filters.append(self.NUMBER_OF_FILTERS[round(reference.filters[i])])
+
+        neurons = []
+        for i in range(len(reference.neurons)):
+            neurons.append(self.NUMBER_OF_NEURONS[round(reference.filters[i])])
+
+        return [conv, dense, filters, neurons]
 
 
 values = [5, 18, 32, 48, 10, 25, 60, 48, 0.0005]
 hyperparameterList = [convolutionLayers, numberOfFilters, denseLayers, numberOfNeurons]
 
 
-def find_closest_hyperparameters(values, hyperparameter_list):
-    closest_list = []
 
-    for value, hyperparameter in zip(values, hyperparameter_list):
-        min_distance = float("inf")  # Positive infinity
-        closest_hyperparameter = None
-
-        for h in hyperparameter:
-            distance = abs(value - h)
-            if distance < min_distance:
-                min_distance = distance
-                closest_hyperparameter = h
-
-        closest_list.append(closest_hyperparameter)
-
-    return closest_list
-
-
-print(find_closest_hyperparameters(values, hyperparameterList))
+# print(find_closest_hyperparameters(values, hyperparameterList))
 
 th = Thesis()
 agents = th.createFirstAgents(5)
 for agent in agents:
+    realValue = th.getHyperparameterList(agent)
+    print(realValue)
     print(agent)
+    print()
